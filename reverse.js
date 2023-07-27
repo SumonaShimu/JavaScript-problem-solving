34730a18c8036409ea5935e0826db77dee8f054b
SumonaShimu <sumona59shimu@gmail.com>
2023-07-27 22:26:03 +0600

    problems added

--- reverse.js
+++ reverse.js
@@ -1,3 +1,4 @@
+//Task 1 : Create a function that takes a string as input and returns the reversed version of the string without using the built-in reverse() method. 
 const reverseString=(str)=>{
     const newArray=str.split("");
     for(let i=0,j=newArray.length-1;i<=j;i++,j--)
@@ -7,7 +8,7 @@ const reverseString=(str)=>{
         newArray[j]=temp;
     }
     const newStr=newArray.join("");
-    console.log(newStr);
+    return newStr;
 }
 const str="hello world"
-reverseString(str)
+console.log(reverseString(str));
---
+++ sumOfArray.js
@@ -0,0 +1,1 @@
+//Create a function that takes an array of numbers as input and returns the sum of all positive numbers in the array. 
